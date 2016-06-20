fdd344ea7ff6a1fc7c56133c2d17f4c3fc01e7ef
gerbenvandervries <gerbenvandervries@gmail.com>
2016-06-20 15:27:02 +0200

    Samplesheet added to resultsDir. uploading log added for rerun demultiplexing.

--- compute5/NGS_Demultiplex/protocols/UploadSampleheet.sh
+++ compute5/NGS_Demultiplex/protocols/UploadSampleheet.sh
@@ -1,5 +1,6 @@
 #MOLGENIS walltime=00:59:00 mem=2gb cores=1
 #string sampleSheet
+#string runResultsDir
 #string MCsampleSheet
 #string workDir
 #string runPrefix
@@ -9,9 +10,21 @@ WHOAMI=$(whoami)
 
 
 echo "Importing Samplesheet into ${MOLGENISSERVER}"
-
 cp ${sampleSheet} ${MCsampleSheet} 
 
-CURLRESPONSE=$(curl -H "Content-Type: application/json" -X POST -d "{"username"="${USERNAME}", "password"="${PASSWORD}"}" https://${MOLGENISSERVER}/api/v1/login)
-TOKEN=${CURLRESPONSE:10:32}
-curl -H "x-molgenis-token:${TOKEN}" -X POST -F"file=@${MCsampleSheet}" -Faction=add -Fnotify=false https://${MOLGENISSERVER}/plugin/importwizard/importFile
+echo "cp ${sampleSheet} to ${runResultsDir}"
+cp ${sampleSheet} ${runResultsDir}
+
+SAMPLESHEETNAME=$( ${sampleSheet} .csv )
+
+if [ ! -f ${workDir}/logs/${SAMPLESHEETNAME}.is.uploaded ]
+then
+
+	CURLRESPONSE=$(curl -H "Content-Type: application/json" -X POST -d "{"username"="${USERNAME}", "password"="${PASSWORD}"}" https://${MOLGENISSERVER}/api/v1/login)
+	TOKEN=${CURLRESPONSE:10:32}
+	curl -H "x-molgenis-token:${TOKEN}" -X POST -F"file=@${MCsampleSheet}" -Faction=add -Fnotify=false https://${MOLGENISSERVER}/plugin/importwizard/importFile
+
+	touch ${workDir}/logs/${SAMPLESHEETNAME}.is.uploaded
+else
+	echo "samplesheet already uploaded to ${MOLGENISSERVER}"
+fi
